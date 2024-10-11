31ab919bd922d9d14946d6464449473549d14ba2
showroomprive <alexis.burgos@showroomprive.net>
2024-10-11 10:20:41 +0200

    feat: Add httpRequestHeader & httpResponseHeader SemanticAttributes

--- Sources/SemanticAttributesWrapper.swift
+++ Sources/SemanticAttributesWrapper.swift
@@ -7,43 +7,62 @@
 
 import OpenTelemetryApi
 
-/// Provides a convenient way to access common OpenTelemetry semantic attributes in Objective-C code.
+/// A Swift enumeration that provides Objective-C compatible access to common OpenTelemetry semantic attributes.
 ///
-/// This enum encapsulates a set of predefined semantic attribute keys, allowing for easy and consistent use of these attributes when instrumenting Objective-C applications.
+/// This wrapper simplifies the use of OpenTelemetry's semantic attribute keys in Objective-C code, offering a structured and type-safe way
+/// to work with attributes like HTTP request headers, methods, URLs, and response status codes when instrumenting applications.
+///
+/// Each case in this enum corresponds to a specific semantic attribute key, which can be used to associate telemetry data with spans.
 @objc public enum SemanticAttributesWrapper: Int, RawRepresentable {
-    /// Represents the HTTP request method attribute, indicating the method used for the HTTP request (e.g., GET, POST, PUT).
+    /// Represents the HTTP request header attribute, capturing key-value pairs sent in the request headers.
+    case httpRequestHeader
+    
+    /// Represents the HTTP request method attribute, indicating the HTTP method (e.g., GET, POST, PUT) used in the request.
     case httpRequestMethod
     
-    /// Represents the full URL attribute, capturing the complete URL of the HTTP request being processed.
+    /// Represents the full URL of the HTTP request, providing the complete URL path, including query parameters.
     case urlFull
     
-    /// Represents the HTTP response status code attribute, indicating the numerical status code returned by the HTTP server.
+    /// Represents the HTTP response header attribute, capturing key-value pairs from the server's response headers.
+    case httpResponseHeader
+    
+    /// Represents the HTTP response status code, capturing the numerical HTTP status code returned by the server (e.g., 200 for success, 404 for not found).
     case httpResponseStatusCode
     
-    /// The raw string value type representing a semantic attribute.
+    /// The raw string value type used to represent a semantic attribute key.
     public typealias RawValue = String
     
-    /// Returns the raw string value of the semantic attribute, which is the actual key used to associate the attribute with telemetry data.
+    /// Returns the raw string value of the semantic attribute. This is the actual key used to tag telemetry data within traces and spans.
+    ///
+    /// For predefined attributes, the raw value maps to OpenTelemetry's semantic conventions.
     public var rawValue: RawValue {
         switch self {
+            case .httpRequestHeader:
+                return "http.request.header"
             case .httpRequestMethod:
                 return SemanticAttributes.httpRequestMethod.rawValue
             case .urlFull:
                 return SemanticAttributes.urlFull.rawValue
+            case .httpResponseHeader:
+                return "http.response.header"
             case .httpResponseStatusCode:
                 return SemanticAttributes.httpResponseStatusCode.rawValue
         }
     }
     
-    /// Initializes a new instance of the enum from a raw string value.
+    /// Initializes a new instance of `SemanticAttributesWrapper` from a raw string value.
     ///
-    /// - Parameter rawValue: The raw string value of the semantic attribute. If the provided value does not match any known semantic attribute, initialization fails.
+    /// - Parameter rawValue: The raw string key that represents a semantic attribute. If the raw value doesn't match any known attribute, the initializer returns `nil`.
     public init?(rawValue: RawValue) {
         switch rawValue {
+            case "http.request.header":
+                self = .httpRequestHeader
             case SemanticAttributes.httpRequestMethod.rawValue:
                 self = .httpRequestMethod
             case SemanticAttributes.urlFull.rawValue:
                 self = .urlFull
+            case "http.response.header":
+                self = .httpResponseHeader
             case SemanticAttributes.httpResponseStatusCode.rawValue:
                 self = .httpResponseStatusCode
             default:
